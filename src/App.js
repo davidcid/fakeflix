97668c2e01d5106ddc3d6c3e4b83dc6d94d60f4e
David Cid <david.cid@gmail.com>
2021-02-15 13:44:35 +0100

    footer component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import { FooterContainer } from './containers/footer';
 import { JumbotronContainer } from './containers/jumbotron';
 
 export default function App() {
   return (
-    <JumbotronContainer />
+    <>
+      <JumbotronContainer />
+      <FooterContainer />
+    </>
   );
 }
