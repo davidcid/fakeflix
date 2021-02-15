283bf5049dd1815d269466fdd9e174fc0568406f
David Cid <david.cid@gmail.com>
2021-02-15 13:05:41 +0100

    modules, errors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
+import 'normalize.css';
 import App from './app';
 import { GlobalStyles } from './global-styles';
 
-render(
-    <>
-        <GlobalStyles /><App />
-    </>,
+render(<>
+    <GlobalStyles />
+    <App />
+</>,
     document.getElementById('root'));
