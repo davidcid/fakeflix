c49d7c15757b627b011b442bfcf147ba35be3b8f
davidcid <david.cid@gmail.com>
2021-02-18 14:34:07 +0100

    preparing singin

---
+++ src/components/form/styles/form.jsx
@@ -0,0 +1,21 @@
+import styled from "styled-components/macro";
+
+import { Link as ReachRouterLink } from "react-router-dom";
+
+export const Container = styled.div``;
+
+export const Error = styled.div``;
+
+export const Base = styled.div``;
+
+export const Title = styled.div``;
+
+export const Container = styled.div``;
+
+export const Container = styled.div``;
+
+export const Container = styled.div``;
+
+export const Container = styled.div``;
+
+export const Container = styled.div``;
