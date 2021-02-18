7e5141c0734db72e5b7f06bd192b25be29678f1b
davidcid <david.cid@gmail.com>
2021-02-18 19:37:23 +0100

    signin wip

--- src/pages/signin.jsx
+++ src/pages/signin.jsx
@@ -1,10 +1,42 @@
+import { useState } from "react";
+
 import { HeaderContainer } from "../containers/header";
 import { FooterContainer } from "../containers/footer";
+import { Form } from "../components";
 
 export default function Signin() {
+  const [emailAddress, setEmailAddress] = useState();
+  const [password, setPassword] = useState();
+  const [error, setError] = useState("");
+
+  const isInvalid = password === "" || emailAddress === "";
+
   return (
     <>
-      <HeaderContainer>Hello from the sign in!</HeaderContainer>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign In</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
+
+          <Form.Base onSubmit={handleSignin} method="POST">
+            <Form.Input
+              placeholder="Email address"
+              value={emailAddress}
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+            <Form.Input
+              type="password"
+              autoComplete="off"
+              placeholder="Password"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <Form.Submit disabled={isInvalid} type="submit">
+              Sign In
+            </Form.Submit>
+          </Form.Base>
+        </Form>
+      </HeaderContainer>
       <FooterContainer />
     </>
   );
