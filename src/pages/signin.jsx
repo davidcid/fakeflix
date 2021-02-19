abb7538f6869317cab035f85f6621f01d75f68ac
David Cid <david.cid@gmail.com>
2021-02-19 17:53:48 +0100

    signin component styled

--- src/pages/signin.jsx
+++ src/pages/signin.jsx
@@ -1,15 +1,21 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { FirebaseContext } from "../context/firebase";
 
 import { HeaderContainer } from "../containers/header";
 import { FooterContainer } from "../containers/footer";
 import { Form } from "../components";
 
 export default function Signin() {
-  const [emailAddress, setEmailAddress] = useState();
-  const [password, setPassword] = useState();
+  const { firebase } = useContext(FirebaseContext);
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
   const [error, setError] = useState("");
 
   const isInvalid = password === "" || emailAddress === "";
+
+  const handleSignin = (event) => {
+    event.preventDefault();
+  };
 
   return (
     <>
@@ -35,6 +41,13 @@ export default function Signin() {
               Sign In
             </Form.Submit>
           </Form.Base>
+          <Form.Text>
+            New to Netflix? <Form.Link to="/signup">Sign up now.</Form.Link>
+          </Form.Text>
+          <Form.TextSmall>
+            This page is protected by Google reCAPTCHA to ensure you're not a
+            bot. Learn more.
+          </Form.TextSmall>
         </Form>
       </HeaderContainer>
       <FooterContainer />
