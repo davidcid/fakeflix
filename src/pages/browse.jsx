51096d2f50a17ed17ef7c6900a665f9a6521c143
David Cid <david.cid@gmail.com>
2021-02-22 22:36:12 +0100

    selection-filter

--- src/pages/browse.jsx
+++ src/pages/browse.jsx
@@ -1,13 +1,11 @@
+import { BrowseContainer } from "../containers/browse";
 import { useContent } from "../hooks";
+import selectionFilter from "../utils/selection-filter";
 
 export default function Browse() {
-  // we need the series and the films
   const { series } = useContent("series");
   const { films } = useContent("films");
-  console.log(series);
-  console.log(films);
-  // we need slides
-  // pass it to the browse container
+  const slides = selectionFilter({ series, films });
 
-  return <p>Hello from the browse!</p>;
+  return <BrowseContainer slides={slides} />;
 }
