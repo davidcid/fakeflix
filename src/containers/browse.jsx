651545144b26b13e6bd9ad5d6c0e8a1c4e7a5f30
davidcid <david.cid@gmail.com>
2021-02-25 11:10:06 +0100

    live search

--- src/containers/browse.jsx
+++ src/containers/browse.jsx
@@ -1,4 +1,5 @@
 import { SelectProfileContainer } from "./profiles";
+import Fuse from "fuse.js";
 import { FirebaseContext } from "../context/firebase";
 import { FooterContainer } from "./footer";
 import { useContext, useEffect, useState } from "react";
@@ -25,6 +26,19 @@ export function BrowseContainer({ slides }) {
   useEffect(() => {
     setSlideRows(slides[category]);
   }, [slides, category]);
+
+  useEffect(() => {
+    const fuse = new Fuse(slideRows, {
+      keys: ["data.description", "data.title", "data.genre"],
+    });
+    const results = fuse.search(searchTerm).map(({ item }) => item);
+
+    if (slideRows.length > 0 && searchTerm.length > 3 && results.length > 0) {
+      setSlideRows(results);
+    } else {
+      setSlideRows(slides[category]);
+    }
+  }, [searchTerm]);
 
   return profile.displayName ? (
     <>
